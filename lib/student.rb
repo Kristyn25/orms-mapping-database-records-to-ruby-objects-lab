5b72d0c99b21c92f0097c245a1180d3fd6df9897
Kristyn <kristynlittle@frontier.com>
2021-10-30 16:46:17 -0700

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -62,7 +62,7 @@ class Student
     end
   end
 
-  def self.first_x_students_in_grade_10(x)
+  def self.first_X_students_in_grade_10(x)
     sql = <<-SQL
       SELECT *
       FROM students
@@ -88,7 +88,7 @@ class Student
     end.first
   end
 
-  def self.all_students_in_grade_x(grade)
+  def self.all_students_in_grade_X(grade)
     sql = <<-SQL
       SELECT *
       FROM students
